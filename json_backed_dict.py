617a68c241d1d5e172cec462516c3d6db3893727
bmaranville <brian.maranville@nist.gov>
2015-09-27 21:11:57 -0400

    making subdicts of JSON-backed dict also write to main dict on update setitem etc.

--- json_backed_dict.py
+++ json_backed_dict.py
@@ -11,7 +11,7 @@ class JSONBackedDict(dict):
         if os.path.exists(self.filename):
             self._read()
         else:
-            self._write()            
+            self._write()
     
     def _write(self):
         fd_out, fd_out_name = tempfile.mkstemp(dir=os.curdir)
@@ -26,11 +26,30 @@ class JSONBackedDict(dict):
             self.clear()
         with open(self.filename, "r") as f:
             self.update(json.loads(f.read()))
+            
+    def __getitem__(self, key):
+        # convert on retrieve: 
+        value = dict.__getitem__(self, key)
+        if isinstance(value, dict) and not isinstance(value, JSONBackedSubDict):
+            value = JSONBackedSubDict(self, input_dict=value)
+            self[key] = value
+        return value
     
-    def __setitem__(self, *args, **kw):
-        dict.__setitem__(self, *args, **kw)
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
         self._write()
     
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) > 1:
+                raise TypeError("update expected at most 1 arguments, "
+                                "got %d" % len(args))
+            other = dict(args[0])
+            for key in other:
+                self[key] = other[key]
+        for key in kwargs:
+            self[key] = kwargs[key]
+            
     def setdefault(self, *args, **kw):
         dict.setdefault(self, *args, **kw)
         self._write()
@@ -39,3 +58,12 @@ class JSONBackedDict(dict):
         val = dict.pop(self, *args, **kw)
         self._write()
         return val
+        
+class JSONBackedSubDict(JSONBackedDict):
+    def __init__(self, root_dict, input_dict=None):
+        input_dict = input_dict or {}
+        dict.__init__(self, input_dict)
+        self.root_dict = root_dict
+        
+    def _write(self):
+        self.root_dict._write()
